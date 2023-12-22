3f37745729160ec8f672e9b27a9a319e2555a210
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-12-22 16:58:01 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const rows = [
+    ...document.querySelectorAll("tr")
+];
+rows.forEach((row)=>{
+    const cell = document.createElement(row.children[1].tagName.toLowerCase());
+    cell.textContent = row.children[1].textContent;
+    row.insertBefore(cell, row.children[row.children.length - 1]);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
